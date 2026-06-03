ca70c75d1f24e3f485d3a097ca1b0068874ef928
estherhabimana <e.habimana@alustudent.com>
2026-06-03 03:06:18 -0700

    Calling the function

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+#Declaing the function
 initialize_system(){
+
+	#Files of interest
 	FILES=("active_logs" "archived_logs" "reports")
+
+	#A for loop that goes through our files of interest with an if nested to check if it exists or not and create it
 	for F in "${FILES[@]}"; do
 		if [ -e "$F" ]; then
 			echo "$F already exists"
@@ -10,3 +16,6 @@ initialize_system(){
 		fi
 	done
 }
+
+#Calling the function to actually execute the code
+initialize_system
